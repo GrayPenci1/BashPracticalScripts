0229036dadf84c77156f178eea580c557283baed
Cain <none>
2025-02-23 17:22:32 +1000

    Add 14-th ex result

--- dirBackup.sh
+++ dirBackup.sh
@@ -3,7 +3,8 @@
 BackupDir() {
 	
 	local FILES_LIST=$(ls $DIR_FOR_BACKUP)
-	tar --force-local -cvf "backup_$(date +"%T-%m-%d").tar" $FILES_LIST
+	# --force-local т.к. в ином случае : воспринимается как разделитель между имя_машины:путь, из-за чего он пытается подключится этой "машине"
+	tar --force-local -cvf "backup_$(date +"%T-%m-%d").tar" $FILES_LIST 
 	echo "$FILES_LIST"
 	exit 0
 
